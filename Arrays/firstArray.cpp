d50146e1b044cb35cea3d50f286f058bea2e4927
Durgesh Shukla <thegdshukla@gmail.com>
2024-02-19 23:46:44 +0530

    seventh  commit

---
+++ Arrays/firstArray.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main(){
+    //int a[5]={1,2,3,4,5};
+    int a[5];
+    for(int i=0;i<5;i++){
+        cin>>a[i];
+    }
+    for(int i=0;i<5;i++){
+        cout<<a[i]<<" ";
+    }
+    cout<<endl;
+    for(int i=4;i>=0;i--){
+        cout<<a[i]<<" ";
+    }
+}
